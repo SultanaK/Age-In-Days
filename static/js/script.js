383b71483fbdd219936a0244d158f911a7404581
Your name here <kr.shampa@yahoo.com>
2020-03-06 01:49:08 -0600

    adding files

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+//Your age in day
+function ageInDays() {
+    
+var birthYear =prompt("Enter your birth year.");
+var ageDays= (2020-birthYear) * 365;
+var h1 =document.createElement('h1');
+var answer=document.createTextNode('You are'+ ageDays +' days');
+h1.setAttribute('id','ageDays');
+h1.appendChild(answer);
+document.getElementById('flex-box-result').appendChild(h1);
+};
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+    
+}
